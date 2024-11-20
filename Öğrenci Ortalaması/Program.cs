0a55cd23207324a3d5216419355f51ef8ff9129e
tubavxk <tubaazsoy@gmail.com>
2024-11-20 20:24:38 +0300

    Öğrenci Ortalaması
    3 dersin ortalamasını alıyor ve geçip geçemediğini yazıyor.

---
+++ Öğrenci Ortalaması/Program.cs
@@ -0,0 +1,22 @@
+﻿int ders1, ders2, ders3;
+Console.Write("1. ders notunu girin: ");
+ders1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. ders notunu girin: ");
+ders2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("3. ders notunu girin: ");
+ders3 = Convert.ToInt32(Console.ReadLine());
+
+
+int ortalama = (ders1 + ders2 + ders3) / 3;
+
+
+if (ortalama > 50)
+{
+    Console.WriteLine("Geçtiniz!");
+}
+else
+{
+    Console.WriteLine("Kaldınız.");
+}
